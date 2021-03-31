a5bf66fac70ede080ab05f6b1e6a379e5ea7e5a0
PietuKanerva <59826957+PietuKanerva@users.noreply.github.com>
2021-03-31 19:46:59 +0300

    Inserted the questions properly

--- pietu/VaalikonePietu/src/main/java/HelloAppEngine.java
+++ pietu/VaalikonePietu/src/main/java/HelloAppEngine.java
@@ -16,90 +16,70 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
-@WebServlet(
-    name = "HelloAppEngine",
-    urlPatterns = {"/questions"}
-)
+@WebServlet(name = "HelloAppEngine", urlPatterns = { "/questions" })
 public class HelloAppEngine extends HttpServlet {
 	model m = new model();
-	
 
-    @Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-    	
-    	// The needed variables for the program
-    	
-    	String driver = "com.mysql.jdbc.Driver";
-		String DBpath = "//localhost/vaalikone";
-		String username = "root";
-		String password = "Zelja";
-		Connection con = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		m.getConnection();
-		
-		
+
 		ArrayList<String> k = new ArrayList<>();
-		
+
 		k = m.getQuestion();
-		
-		
+
 		response.setContentType("text/plain");
-    	response.setCharacterEncoding("UTF-8");   	
-    	response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        
-        out.println("<p>");
-        response.getWriter().print("Election Machine\r\n");
-        out.println("<br>");
-        response.getWriter().print("Questions\r\n");
-        
-        out.println("</p>");
-        out.println("<html>");
-        out.println("<body>");
-        out.println("<ul>");
-        
-        String value;
-        
-        
-        int index = 1;
-		for (String x : k) {
-			out.println("<li>"+ String.valueOf(index++)+": "+ x + "</li>");
-        }
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		out.println("<p>");
+		response.getWriter().print("Election Machine\r\n");
+		out.println("<br>");
+		response.getWriter().print("Questions\r\n");
+
+		out.println("</p>");
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<ul>");
 		
 		out.println("<p><form action = \"/submit\" method=\"GET\"");
-		index = 1;
+		out.println("</p>");
+		
+		int index = 1;
 		for (int i = 0; i < 19; i++) {
 			out.println("<li>" + String.valueOf(index++));
-			out.println("<input type = \"radio\" name = \"Q"+i+"rad\" value = \"1\"> Täysin eri mieltä");
-			out.println("<input type = \"radio\" name = \"Q"+i+"rad\" value = \"2\">  Eri mieltä");
-			out.println("<input type = \"radio\" name = \"Q"+i+"rad\" value = \"3\"> Neutraali");
-			out.println("<input type = \"radio\" name = \"Q"+i+"rad\" value = \"4\"> Samaa mieltä");
-			out.println("<input type = \"radio\" name = \"Q"+i+"rad\" value = \"5\"> Täysin samaa mieltä");
+			out.println(k.get(i));
+			out.println("<p>");
+			out.println("</p>");
+			out.println("<input type = \"radio\" name = \"Q" + i + "rad\" value = \"1\"> Täysin eri mieltä");
+			out.println("<input type = \"radio\" name = \"Q" + i + "rad\" value = \"2\">  Eri mieltä");
+			out.println("<input type = \"radio\" name = \"Q" + i + "rad\" value = \"3\"> Neutraali");
+			out.println("<input type = \"radio\" name = \"Q" + i + "rad\" value = \"4\"> Samaa mieltä");
+			out.println("<input type = \"radio\" name = \"Q" + i + "rad\" value = \"5\"> Täysin samaa mieltä");
+			out.println("<p>");
+			out.println("</p>");
 			out.println("</li>");
 		}
 		
+		out.println("</ul>");
+
 		ArrayList<String> vastaukset = new ArrayList<>();
-		
-		/* for (int i = 0; i <20; i++) {
+
+		for (int y = 0; y < 19; y++) {
 			String value1 = request.getParameter("Q1rad");
-			vastaukset.add(value1);	
+			vastaukset.add(value1);
 			response.getWriter().print(value1);
 		}
-		
 
 		out.println("<input type=\"submit\" value=\"send\">");
-		
+
 		out.println("</form> </p>");
-		
+
 		out.println("</html>");
-        out.println("</body>");
-        out.println("</ul>");
-        */
-		
+		out.println("</body>");
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -109,6 +89,3 @@ public class HelloAppEngine extends HttpServlet {
 	}
 
 }
-
-    
-  
